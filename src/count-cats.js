0c482657e7e17ca1df97a4c1c80e5520a4a66ac8
Yurii111111 <56649945+Yurii111111@users.noreply.github.com>
2020-09-24 23:28:27 +0300

    Count cats!

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,12 +1,5 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(matrix) {
-  matrix = matrix.reduce((a, b) => a.concat(b));
-  let sum = [];
-  for (let i = 0; i < matrix.length; i++) {
-    if (matrix[i] == "^^")
-      sum.push(i);
-  }
-  return sum.length
-  
+  return matrix = [].concat(...matrix).filter(a => a == "^^").length;
 };
