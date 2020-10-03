d0c3f7ae25a0ed6ccc55a75bbe891593c9e2d3d6
Yurii Verbitskii <km1111111@yandex.ru>
2020-10-04 00:21:27 +0300

    feat: add task solution simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,46 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.push(value);
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (typeof (position) !== 'number') {
+      this.chain = [];
+      throw new Error();
+    }
+
+    position--;
+    if (position < 0 || this.getLength() <= position) {
+      this.chain = [];
+      throw new Error();
+    }
+
+    this.chain.splice(position, 1);
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let string = '';
+
+    for (let i = 0; i < this.getLength(); i++) {
+      string += `( ${this.chain[i]} )`;
+
+      if (i < this.getLength() - 1) {
+        string += '~~';
+      }
+    }
+
+    this.chain = [];
+    return string;
   }
 };
 
