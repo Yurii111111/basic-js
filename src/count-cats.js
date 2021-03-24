c568535fc8f885749b71149bf6552449c12dba99
Yurii Verbitskii <km1111111@yandex.ru>
2021-03-24 22:28:45 +0300

    add: add solution extended-repeater

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,5 +1,7 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(matrix) {
-  return matrix = [].concat(...matrix).filter(a => a == "^^").length;
+  // return matrix = [].concat(...matrix).filter(a => a == "^^").length;
+  return matrix = matrix.reduce((a, b) => a.concat(b), []).filter(a => a == "^^").length;
+  
 };
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,36 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  str = String(str);
+  if ("addition" in options) { options.addition = String(options.addition); }
+  if (options.repeatTimes == undefined) { options.repeatTimes = 1 }
+  if (options.additionRepeatTimes == undefined) { options.additionRepeatTimes = 1 }
+  let array = [];
+  let additionArray = [];
+  let additionResult = '';
+  let result = str;
+  if (options.repeatTimes) {
+    for (let i = 0; i < options.repeatTimes; i++) {
+      if (options.addition) {
+        for (let j = 0; j < options.additionRepeatTimes; j++) {
+          additionArray.push(options.addition);
+        }
+        if (options.additionSeparator) {
+          additionResult = additionArray.join(`${options.additionSeparator}`);
+        } else {
+          additionResult = additionArray.join('|');
+        }
+        additionArray = [];
+      }
+      array.push(str + additionResult);
+    }
+    if (options.separator) {
+      result = array.join(`${options.separator}`);
+    } else {
+      result = array.join('+');
+    }
+    array = [];
+  }
+  return result;
 };
   
